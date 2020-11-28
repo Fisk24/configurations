91b9455a5b5820daba272fbbfd3562695cc78d97
Fisk <mcfisk@ymail.com>
2020-11-28 11:47:48 -0600

    Reduced scope of server-network-config.sh

--- Server/server-network-config.sh
+++ Server/server-network-config.sh
@@ -5,19 +5,31 @@ GATEWAY=10.0.0.1    # The Network Gateway IP: This is the IP address of the rout
 DNS=10.0.0.1        # DNS Server IP: This is usually the same as the gateway but in some cases it may be neccisary to change it. This script uses this value in addition to google's public DNS (8.8.8.8)
 
 echo "Installing manditory packages. Ensure that you have an active network connection before running this script"
-pacman -S openssh networkmanager git vim
+pacman -S openssh networkmanager git vim python
 
+# Stop/Disable systemd-networkd and systemd-resolved
+
+sudo systemctl stop systemd-networkd systemd-resolved
+sudo systemctl disable systemd-networkd systemd-resolved
 
 # Start and Enable NetworkManager.service
 echo "Begin NetworkManager"
-#sudo systemctl start NetworkManager
-#sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+sudo systemctl enable NetworkManager
 
 echo "Configuring NetworkManager"
-connameraw=`nmcli -f NAME con show`
-conname=${connameraw:5}
-echo $conname
+./nmconfig.py $IP $GATEWAY $DNS
+echo "Static IP established..."
+echo "Completed Configuring NetworkManager"
 
 # Start and Enable sshd.service
+echo "Begin sshd"
+sudo systemctl start sshd
+sudo systemctl enable sshd
 
-# Establish static ip address, settable from $IP variable
+echo "SSH is up on port 22. Change this immediatly"
+
+echo "Basic network configuration complete!"
+echo "Please reboot the server, for the changes to take effect."
+
+
